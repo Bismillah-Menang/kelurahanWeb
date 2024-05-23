c5477e17a431c3f31d675be6de0af92831b38f2b
aimeennnn <muh.aiman21304@gmail.com>
2024-05-23 09:03:28 +0700

    ADD = Dashboard Admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -9,7 +10,8 @@ class DashboardController extends Controller
 {
     function dashboardindex()
     {
-    return view ('frontend.dashboard');
+        $data = User::where('role','user')->get();
+        return view ('frontend.dashboard',compact('data'));
     }
     function logout()
     {
